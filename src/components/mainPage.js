e76b0fa84d0013c93255b14133dde8dc2d17acad
Shawn McGinty <mcginty.shawn@gmail.com>
2020-08-07 21:40:45 -0500

    added matomo and favicon

--- src/components/mainPage.js
+++ src/components/mainPage.js
@@ -6,6 +6,19 @@ import Tmux from './tmux';
 
 import './mainPage.css';
 
+/*  Matomo */
+var _paq = window._paq = window._paq || [];
+/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+	var u="//matomo.shawnmcginty.com/";
+	_paq.push(['setTrackerUrl', u+'matomo.php']);
+	_paq.push(['setSiteId', '3']);
+	var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+	g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+})();
+/* End Matomo Code */
 export default function MainPage({ display }) {
 	return <div className="flex flex-col h-screen max-h-screen min-h-0">
 		<Helmet>
